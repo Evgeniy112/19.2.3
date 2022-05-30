c502ac71602ecb2fbafdd5eb89aca98df6b0dda9
Evgeniy112 <evgeniy.pogorelov@bk.ru>
2022-05-30 22:17:56 +0300

    calctest

---
+++ tests/calctest.py
@@ -0,0 +1,32 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_division_calculate_correctly(self):
+        assert int(self.calc.division(self, 2, 2)) == 1
+
+    def test_division_calculate_failed(self):
+        assert int(self.calc.division(self, 2, 2)) != 1
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 6, 2) == 4
+
+    def test_subtraction_calculate_failed(self):
+        assert self.calc.subtraction(self, 7, 2) == 72
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 6, 2) == 8
+
+    def test_adding_calculate_failed(self):
+        assert self.calc.adding(self, 7, 2) == 15
+
